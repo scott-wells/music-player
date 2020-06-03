7be8fae7f11decf39966b155cccde166227dcd0b
Scott Wells <efwells101@gmail.com>
2020-06-03 13:47:33 -0600

    Create new app. Install dependencies.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='outer-wrap'>
+      <div className='App'>
+        <div className='navbar'>
+          <div className='logo'>logo</div>
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Your Library</li>
+        </ul>
+      </div>
+      <div className='music-controls'>music controls</div>
     </div>
   );
 }
