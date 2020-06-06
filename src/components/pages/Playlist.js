cab743ba238b53816a94c9a4b09ae729444ea1de
Scott Wells <efwells101@gmail.com>
2020-06-06 16:24:34 -0600

    Style 'Playlist' component.

--- src/components/pages/Playlist.js
+++ src/components/pages/Playlist.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ReactComponent as PlayIcon } from "../../svg/play.svg";
+import { ReactComponent as HeartIcon } from "../../svg/heart.svg";
+import { ReactComponent as NoteIcon } from "../../svg/note.svg";
 
 const PlaylistPage = () => {
   let { id } = useParams();
@@ -8,14 +10,14 @@ const PlaylistPage = () => {
   return (
     <div className='playlist-page'>
       <div className='main-inner'>
-        <div className='playlist-page-info'>
+        <div className='playlist-page-header'>
           <div className='playlist-page-image'>
             <img
               src='https://images.unsplash.com/photo-1573255437415-ea46be7057d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
               alt='city neon'
             />
           </div>
-          <div className='playlist-page-content'>
+          <div className='playlist-page-title'>
             <p className='small-text uppercase bold'>Playlist</p>
             <h1>A Perfect Day</h1>
             <p className='tagline'>
@@ -28,21 +30,88 @@ const PlaylistPage = () => {
           </div>
         </div>
         <div className='playlist-page-songs'>
-          <div className='play-list-buttons'>
-            <span className='play-icon'>
+          <div className='playlist-buttons'>
+            <span className='icon play-icon'>
               <PlayIcon />
             </span>
-            <div className='icons'>
-              <div className='icons-heart'></div>
-              <div className='icons-dots'></div>
-            </div>
+            <span className='icon heart-icon'>
+              <HeartIcon />
+            </span>
+            <span className='icon dots-icon'>•••</span>
           </div>
-          <ul>
-            <li>Song one</li>
-            <li>Song two</li>
-            <li>Song three</li>
-            <li>Song four</li>
-            <li>Song five</li>
+          <ul className='song-list'>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
+            <li>
+              <div className='song-icon'>
+                <NoteIcon />
+              </div>
+              <div className='song-details'>
+                <h3>Held Down</h3>
+                <span>Laura Marling</span> • <span>Held Down</span>
+              </div>
+              <div className='song-time'>
+                <span>4:07</span>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
